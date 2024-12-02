606813599ab1aa05e89f7c55fd18dfc7a70149d9
savhannab <156967796+savhannab@users.noreply.github.com>
2024-12-01 19:31:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 //Saved recipes container
 var savedRecipesGrid = query.Selector('.saved-recipes .recipes-grid');
+
+//Function to add new recipe to saved list
+function addSavedRecipe(recipe) {
+  var recipeCard = document.createElement('div');
+}
+
+//Function to remove recipe
+
+//Example recipes
+
+//Function to filter recipes by search criteria
+
+//Event listeners
